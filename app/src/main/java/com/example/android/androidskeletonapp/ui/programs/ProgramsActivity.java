c628e5016113e4c687c04d30f60100ea5606b7f9
Carlos Djedje <cdjedje18@gmail.com>
2019-07-02 22:48:33 -0700

    Programs list

--- app/src/main/java/com/example/android/androidskeletonapp/ui/programs/ProgramsActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/programs/ProgramsActivity.java
@@ -2,9 +2,15 @@ package com.example.android.androidskeletonapp.ui.programs;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.LiveData;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.GridLayoutManager;
+
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
 
+import org.hisp.dhis.android.core.program.Program;
 import org.hisp.dhis.android.core.program.ProgramType;
 
 public class ProgramsActivity extends ListActivity implements OnProgramSelectionListener {
@@ -14,6 +20,18 @@ public class ProgramsActivity extends ListActivity implements OnProgramSelection
         super.onCreate(savedInstanceState);
         setUp(R.layout.activity_programs, R.id.programsToolbar, R.id.programsRecyclerView);
         // TODO Show program list
+        ProgramsAdapter adapter = new ProgramsAdapter(this);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(adapter);
+
+        LiveData<PagedList<Program>> programs = Sdk.d2().programModule().programs
+                .withProgramStages()
+                .withStyle()
+                .getPaged(20);
+
+        programs.observe(this, programPagedList -> {
+            adapter.submitList(programPagedList);
+        });
     }
 
     @Override
